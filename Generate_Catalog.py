6832a90db50907ff61d593d2c817b2a7e08216ee
Staskaer <liujiaxin011121@gmail.com>
2023-12-05 14:02:44 +0800

    catalog

--- Generate_Catalog.py
+++ Generate_Catalog.py
@@ -1,11 +1,4 @@
-raw_text = """# 这是什么？
 
-目前用了到esp-idf，但是API不太好记，而且官方例程很长，所以就基于例程把大部分常用的API提取出来，方便自己查阅。
-
----
-
-# 目录
-"""
 
 title_list = []
 
@@ -22,8 +15,16 @@ with open('Reference.md', 'r', encoding="UTF-8") as f:
             title = line.strip('#').strip()
             title_list.append((title, title_level))
 
-with open("README.md", 'w', encoding="UTF-8") as f:
-    f.write(raw_text+"\n\n")
+with open("README.md", 'r+', encoding="UTF-8") as f:
+    # 先定位到目录行再写入
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        if "目录" in line:
+            f.seek(f.tell())
+            break
+    f.write("\n\n")
     for title, title_level in title_list:
         f.write("  " * (title_level - 1) + "* [" + title + "](./Reference.md#" + title.replace(" ", "-").replace(":", "").replace("?", "").replace("!", "").replace(".", "").
                 replace(",", "").replace(";", "").replace("(", "").replace(")", "").replace("/", "").replace("\\", "").replace("'", "").replace('"', "").lower() + ")\n")
